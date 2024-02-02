b5c63cfebb16615d0021abce04a1be27e4fd83c7
josue <renteriajosue17@gmail.com>
2024-02-01 20:39:04 -0600

    feat(command): Detach commands into a new file

---
+++ commands/commands.js
@@ -0,0 +1,23 @@
+const ImagesService = require("./services");
+
+const imagesService = new ImagesService();
+
+const helloCommand = (msg) => {
+    msg.reply(`Hello ${msg.author.username}, have a good day!`);
+};
+
+const memeCommand = async(msg) => {
+    msg.channel.send("Here's your meme!");
+    const imgMeme = await imagesService.getMemes();
+    msg.channel.send(imgMeme);
+}
+
+const dayCommand = (msg) => {
+    msg.reply(`Hello today is ${dayjs().format("DD-MMMM-YYYY")}`);
+}
+
+module.exports = {
+    hello: helloCommand,
+    meme: memeCommand,
+    day: dayCommand,
+};

9d46673a8a6e588f0964e0e8880fbe4b93a695fa
josue <renteriajosue17@gmail.com>
2024-02-03 21:57:01 -0600

    feat(channels): Aggregating the dog command

--- channels/channels.js
+++ channels/channels.js
@@ -1,7 +1,7 @@
 module.exports = {
     general: {
         token: process.env.CHANNEL1_TOKE,
-        allowedCommands: ["hello", "day"]
+        allowedCommands: ["hello", "day", "dog"]
     },
     memes: {
         token: process.env.CHANNEL2_TOKE,
